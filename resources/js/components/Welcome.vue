383bc06a7033c82f84f75a824c5ecc2171b4a857
khemraj <khemraj@rwit.io>
2025-06-13 19:03:37 +0530

    install Vue js

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,15 @@
+<<template lang="eng">
+  <div>
+    <h1>Welcome to Laravel with Vue JS!</h1>
+  </div>
+</template>
+<script>
+export default {
+  name:'Welcome'
+}
+</script>
+<style scoped>
+  h1{
+      color:red;
+  }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -11,3 +10,5 @@ export default defineConfig({
         tailwindcss(),
     ],
 });
+
+mix.js('resources/js/app.js', 'public/js').vue();
