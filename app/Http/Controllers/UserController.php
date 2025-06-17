6b9c1c23c4128bd4f384e012fc1b826d62d2ffbb
khemraj <khemraj@rwit.io>
2025-06-17 18:58:28 +0530

    Create api for Delete User and create User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -29,7 +30,23 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
     }
 
     /**
@@ -46,7 +63,7 @@ class UserController extends Controller
     public function edit($id)
     {
         $user = User::find($id);
-        if(is_null($user)){
+        if (is_null($user)) {
             return response()->json(['error' => 'User not found'], 404);
         }
         return $user;
@@ -57,24 +74,38 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $validated = $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|email',
         ]);
-    
-        $user = User::findOrFail($id);
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        
-        $user->save();
-    
-        return response()->json(['message' => 'User updated successfully']);
+
+        if ($validator->fails()) {
+            return response($validator->messages(), 422);
+        } else {
+
+            $user        = User::findOrFail($id);
+            $user->name  = $request->input('name');
+            $user->email = $request->input('email');
+
+            $user->save();
+
+            return response()->json(['message' => 'User updated successfully']);
+        }
     }
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $users)
+    public function destroy(User $users, $id)
     {
-        //
+
+        $user = User::find($id);
+
+        if (is_null($user)) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully'], 200);
     }
 }
